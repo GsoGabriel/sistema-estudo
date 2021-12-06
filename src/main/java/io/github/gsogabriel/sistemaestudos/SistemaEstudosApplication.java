12ecfa6b09c345f62d06a019f58234e01c998d59
unknown <gabriel.sorj@gmail.com>
2021-12-06 15:49:46 -0300

    First commit

---
+++ src/main/java/io/github/gsogabriel/sistemaestudos/SistemaEstudosApplication.java
@@ -0,0 +1,13 @@
+package io.github.gsogabriel.sistemaestudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEstudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEstudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gsogabriel/sistemaestudos/SistemaEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gsogabriel.sistemaestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEstudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
